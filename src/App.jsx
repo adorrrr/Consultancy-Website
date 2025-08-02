79fd3e57cc14722ab9762532da17e3e1f7e9826a
Ahasanul Monir Adar <ahasanulmoniradar@gmail.com>
2025-08-02 22:57:02 +0600

    some

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 import '../src/assets/css/style.scss';
+import './assets/css/style.scss';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './components/frontend/Home';
